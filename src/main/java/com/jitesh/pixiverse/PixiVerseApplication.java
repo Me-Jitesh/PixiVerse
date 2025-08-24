6cdfded809207357dbfa2b49092fad7a30851fb7
Jitesh Singh <jiteshs101@gmail.com>
2025-08-24 21:28:53 +0530

    Basic Structure

---
+++ src/main/java/com/jitesh/pixiverse/PixiVerseApplication.java
@@ -0,0 +1,13 @@
+package com.jitesh.pixiverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixiVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PixiVerseApplication.class, args);
+    }
+
+}
