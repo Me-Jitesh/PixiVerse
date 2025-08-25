06bf3ba1184fd879b1df5daa39c6131add16a384
Jitesh Singh <jiteshs101@gmail.com>
2025-08-25 12:51:10 +0530

    Configuration Completed

--- src/main/java/com/jitesh/pixiverse/PixiVerseApplication.java
+++ src/main/java/com/jitesh/pixiverse/PixiVerseApplication.java
@@ -2,8 +2,10 @@ package com.jitesh.pixiverse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PixiVerseApplication {
 
     public static void main(String[] args) {
